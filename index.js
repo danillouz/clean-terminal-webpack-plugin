ef8c3bdb7084736a47d4de547f764efacd16521f
Daniël Illouz <daniel.illouz@me.com>
2017-10-11 11:22:48 +0200

    Update webpack peer dependency to v3
    Format code using prettier.

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 'use strict';
 
-function CleanTerminalPlugin(options = { }) {
-	const {
-		message
-	} = options;
+function CleanTerminalPlugin(options = {}) {
+  const { message } = options;
 
-	this.message = message;
+  this.message = message;
 }
 
-CleanTerminalPlugin.prototype.apply = function (compiler) {
-	compiler.plugin(
-		'emit',
-		(_, done) => {
-			if (process.env.NODE_ENV !== 'production') {
-				const clear = '\x1B[2J\x1B[3J\x1B[H';
-				const output = this.message ? clear + this.message + '\n\n' : clear;
+CleanTerminalPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('emit', (_, done) => {
+    if (process.env.NODE_ENV !== 'production') {
+      const clear = '\x1B[2J\x1B[3J\x1B[H';
+      const output = this.message ? clear + this.message + '\n\n' : clear;
 
-				process.stdout.write(output);
-			}
+      process.stdout.write(output);
+    }
 
-			done();
-		}
-	);
+    done();
+  });
 };
 
 module.exports = CleanTerminalPlugin;
