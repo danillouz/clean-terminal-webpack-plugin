f0732d29d2ca7b0cc6014c8a0b2693284735dc3f
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-01-03 19:13:48 -0500

    add option to clear terminal in watch mode only

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-"use strict";
+'use strict';
 
 class CleanTerminalPlugin {
   constructor(options = {}) {
     this.message = options.message;
+    this.onlyInWatchMode = options.onlyInWatchMode;
   }
 
   apply(compiler) {
     if (compiler.hooks) {
-      compiler.hooks.afterCompile.tap("CleanTerminalPlugin", () => {
-        if (process.env.NODE_ENV !== "production") this.clearConsole();
+      compiler.hooks.afterCompile.tap('CleanTerminalPlugin', () => {
+        if (this.shouldClearConsole(compiler)) this.clearConsole();
       });
     } else {
       // backwards compatible version of compiler.hooks
-      compiler.plugin("emit", (_, done) => {
-        if (process.env.NODE_ENV !== "production") this.clearConsole();
+      compiler.plugin('emit', (_, done) => {
+        if (this.shouldClearConsole()) this.clearConsole();
         done();
       });
     }
   }
 
+  shouldClearConsole(compiler) {
+    if (this.onlyInWatchMode) return compile.watchMode;
+    else
+      return (
+        process.env.NODE_ENV !== 'production' &&
+        process.env.options.mode !== 'production'
+      );
+  }
+
   clearConsole() {
-    const clear = "\x1B[2J\x1B[3J\x1B[H";
-    const output = this.message ? clear + this.message + "\n\n" : clear;
+    const clear = '\x1B[2J\x1B[3J\x1B[H';
+    const output = this.message ? clear + this.message + '\n\n' : clear;
     process.stdout.write(output);
   }
 }
